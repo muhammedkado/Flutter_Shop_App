1b4205de28c731e91a7d80b8a76272a2b5bbd517
muhammedkado <mehmetkado9@gmail.com>
2022-10-14 17:40:58 +0300

    add toast messages in the login screen

--- lib/modules/login_screen/cubit/cubit.dart
+++ lib/modules/login_screen/cubit/cubit.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_app/models/login_model.dart';
 import 'package:shop_app/modules/login_screen/cubit/state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shop_app/shared/network/end_point.dart';
 import 'package:shop_app/shared/network/remote/dio_helper.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitialState());
 
   static LoginCubit get(context) => BlocProvider.of(context);
-
+  late LoginModel loginModel;
   void userLogin({
     required String email,
     required String password,
   }) {
     emit(LoginLodingState());
-    print('email:$email');
-    print('password:$password');
+
     DioHelper.postData(
       url: 'login',
       data: {
@@ -24,10 +23,10 @@ class LoginCubit extends Cubit<LoginState> {
         "password": password,
       },
     ).then((value) {
-      print('email:$email');
-      print('password:$password');
-      emit(LoginSuccessState());
       print(value.data);
+      loginModel = LoginModel.fromJson(value.data);
+      emit(LoginSuccessState(loginModel));
+
     }).catchError((onError) {
       print(onError.toString());
 
@@ -35,13 +34,12 @@ class LoginCubit extends Cubit<LoginState> {
     });
   }
 
-  IconData suffix=Icons.visibility_outlined;
- bool isPassword=true;
-   ChangePasswordVisibility(){
-     isPassword=!isPassword;
-    suffix=isPassword ? Icons.visibility_outlined:Icons.visibility_off_outlined;
+  IconData suffix = Icons.visibility_outlined;
+  bool isPassword = true;
+  ChangePasswordVisibility() {
+    isPassword = !isPassword;
+    suffix =
+        isPassword ? Icons.visibility_outlined : Icons.visibility_off_outlined;
     emit(LoginChangePasswordVisibilityState());
-
   }
-
 }
--- lib/modules/login_screen/cubit/state.dart
+++ lib/modules/login_screen/cubit/state.dart
@@ -1,7 +1,14 @@
+import 'package:shop_app/models/login_model.dart';
+
 abstract class LoginState {}
 class LoginInitialState extends LoginState{}
 class LoginLodingState extends LoginState{}
-class LoginSuccessState extends LoginState{}
+class LoginSuccessState extends LoginState{
+  final LoginModel loginModel;
+
+  LoginSuccessState(this.loginModel);
+
+}
 class LoginErrorState extends LoginState{
   final String Error;
 
