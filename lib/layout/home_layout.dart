23fdc1d8385a9c1427ff86aad480c501344a8afa
muhammedkado <mehmetkado9@gmail.com>
2022-10-15 15:48:20 +0300

    build layout and cuibt

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/modules/login/login_screen.dart';
+import 'package:shop_app/modules/search/search_screen.dart';
 import 'package:shop_app/shared/components/components.dart';
 import 'package:shop_app/shared/network/local/cashe_helper.dart';
 
-
 class Home_Layout extends StatelessWidget {
-   Home_Layout({Key? key}) : super(key: key);
+  Home_Layout({Key? key}) : super(key: key);
   CachHelper? token;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('ShopApp'),
+        title: const Text('ShopApp'),
         actions: [
-          IconButton(onPressed: ()
-          {
-            CachHelper.removeData(key: 'token').then((value)
-            {
-            if(value)
-              {
-                NavigatorAndFinish(context: context, Widget: Login_Screen());
-                ShowTost(msg: 'تم تسجيل الخروج بنجاح', state: TostState.SUCCESS);
-              }
-            }
-            );
-          }, icon: Icon(Icons.logout)),
+          IconButton(
+              onPressed: () {
+                NavigatorAndFinish(context: context, Widget: SearchScreen());
+                ShowTost(msg: 'search screen', state: TostState.WARNING);
+              },
+              icon: Icon(Icons.search)),
         ],
       ),
+
+
     );
   }
 }
