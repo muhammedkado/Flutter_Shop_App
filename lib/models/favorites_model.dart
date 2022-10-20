a1b8b2affa27283e9724b40b10664c87b80ef23f
muhammedkado <mehmetkado9@gmail.com>
2022-10-21 00:45:23 +0300

    Completion of the Favorites page

--- lib/models/favorites_model.dart
+++ lib/models/favorites_model.dart
@@ -1,46 +1,83 @@
 class Favorites {
+  bool? status;
+  dynamic message;
+  Data? data;
 
-  late final bool status;
-  late final Null message;
-  late final Data data;
 
-  Favorites.fromJson(Map<String, dynamic> json){
-    status = json['status'];
-    message = null;
-    data = Data.fromJson(json['data']);
+  Favorites.fromJson(Map<String, dynamic> json) {
+    status = json['status'] as bool?;
+    message = json['message'];
+    data = (json['data'] as Map<String,dynamic>?) != null ? Data.fromJson(json['data'] as Map<String,dynamic>) : null;
   }
 
 
 }
 
 class Data {
+  int? currentPage;
+  List<FavoritesData>? data;
+  String? firstPageUrl;
+  int? from;
+  int? lastPage;
+  String? lastPageUrl;
+  dynamic nextPageUrl;
+  String? path;
+  int? perPage;
+  dynamic prevPageUrl;
+  int? to;
+  int? total;
 
-  late final int currentPage;
-  late final List<dynamic> data;
-  late final String firstPageUrl;
-  late final Null from;
-  late final int lastPage;
-  late final String lastPageUrl;
-  late final Null nextPageUrl;
-  late final String path;
-  late final int perPage;
-  late final Null prevPageUrl;
-  late final Null to;
-  late final int total;
 
-  Data.fromJson(Map<String, dynamic> json){
+  Data.fromJson(Map<String, dynamic> json) {
     currentPage = json['current_page'];
-    data = List.castFrom<dynamic, dynamic>(json['data']);
-    firstPageUrl = json['first_page_url'];
-    from = null;
+    data = (json['data'] as List?)?.map((dynamic e) => FavoritesData.fromJson(e as Map<String,dynamic>)).toList();
+    firstPageUrl = json['first_page_url'] ;
+    from = json['from'];
     lastPage = json['last_page'];
     lastPageUrl = json['last_page_url'];
-    nextPageUrl = null;
+    nextPageUrl = json['next_page_url'];
     path = json['path'];
     perPage = json['per_page'];
-    prevPageUrl = null;
-    to = null;
+    prevPageUrl = json['prev_page_url'];
+    to = json['to'];
     total = json['total'];
+  }
+
+
+}
+
+class FavoritesData {
+  int? id;
+  Product? product;
+
+
+  FavoritesData.fromJson(Map<String, dynamic> json) {
+    id = json['id'] as int?;
+    product = (json['product'] as Map<String,dynamic>?) != null ? Product.fromJson(json['product'] as Map<String,dynamic>) : null;
+  }
+
+
+}
+
+class Product {
+  int? id;
+  dynamic price;
+  dynamic oldPrice;
+  dynamic discount;
+  String? image;
+  String? name;
+  String? description;
+
+
+
+  Product.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    price = json['price'] ;
+    oldPrice = json['old_price'] ;
+    discount = json['discount'];
+    image = json['image'] ;
+    name = json['name'] ;
+    description = json['description'];
   }
 
 }
--- lib/modules/favorites/favorites_screen.dart
+++ lib/modules/favorites/favorites_screen.dart
@@ -1,11 +1,127 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/layout/cuibt/cuibt.dart';
+import 'package:shop_app/layout/cuibt/states.dart';
+import 'package:shop_app/models/favorites_model.dart';
+import 'package:shop_app/shared/styles/colors.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Center(child: Text('Favorites',style: Theme.of(context).textTheme.bodyLarge,));
+    return BlocConsumer<HomeCuibt, HomeState>(
+        builder: (context, state) {
+          return ConditionalBuilder(
+            condition: state is! FavoritesLoadingState,
+            builder:(context)=> ListView.separated(
+                itemBuilder: (context, index) => favoritesItem(
+                    HomeCuibt.get(context).favorites!.data!.data![index],context),
+                separatorBuilder: (context, index) => SizedBox(
+                      height: 1,
+                      child: Container(
+                        height: 0.5,
+                        width: double.infinity,
+                        color: Colors.black,
+                      ),
+                    ),
+                itemCount:
+                    HomeCuibt.get(context).favorites!.data!.data!.length),
+            fallback:(context)=>const Center(child: CircularProgressIndicator()),
+          );
+        },
+        listener: (context, state) {});
   }
+
+  Widget favoritesItem(FavoritesData favoritesData ,context) => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SizedBox(
+          width: double.infinity,
+          height: 120,
+          child: Row(
+            children: [
+              Stack(
+                alignment: Alignment.bottomLeft,
+                children: [
+                  Image(
+                    image: NetworkImage('${favoritesData.product!.image}'),
+                    fit: BoxFit.cover,
+                    height: 120,
+                    width: 120,
+                  ),
+                  if (favoritesData.product!.discount != 0)
+                    Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 1),
+                        color: Colors.red,
+                        child: const Text(
+                          'DISCOUNT',
+                          style: TextStyle(color: Colors.white, fontSize: 10),
+                        ))
+                ],
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      ' ${favoritesData.product!.name}',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontSize: 14, height: 1.1),
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${favoritesData.product!.price}',
+                          style: const TextStyle(
+                            color: defaultColor,
+                            fontSize: 12.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        if (favoritesData.product!.discount != 0)
+                          Text(
+                            '${favoritesData.product!.oldPrice.round()}',
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 11,
+                                decoration: TextDecoration.lineThrough),
+                          ),
+                        const Spacer(),
+                        IconButton(
+                            onPressed: () {
+                               HomeCuibt.get(context).changeFavorites(favoritesData.product!.id);
+                              //  print(model.id);
+                            },
+                            icon: CircleAvatar(
+                              radius: 13,
+                              backgroundColor:
+                                  HomeCuibt.get(context).favorits[favoritesData.product!.id]!
+                                      ? defaultColor
+                                      : Colors.grey,
+                              child: const Icon(
+                                Icons.favorite_border,
+                                color: Colors.white,
+                                size: 14,
+                              ),
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
 }
